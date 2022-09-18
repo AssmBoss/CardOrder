2f9cd49107faee98c352990a1c8e0b7619c1924c
home <home@MSK-NB-POZ2.ad.kdl-test.ru>
2022-09-18 21:31:04 +0300

    Add tests for checkbox and nonfilling fields

--- src/test/java/ru/netology/cardorder/CardOrderTest.java
+++ src/test/java/ru/netology/cardorder/CardOrderTest.java
@@ -8,33 +8,34 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
 
     @BeforeEach
-    void siteOpen(){
-
+    void siteOpen() {
+        open("http://localhost:9999");
     }
+
     @Test
     void shouldGetSuccessMessageAfterCorrectFilling() {
-        open("http://localhost:9999");
+        Configuration.holdBrowserOpen = true;
         SelenideElement form = $("[enctype='application/x-www-form-urlencoded']");
-        form.$("[name='name']").setValue("Иван Иванов");
+        form.$("[name='name']").setValue("Анна-Мария Иванова-Редгрейв");
         form.$("[name='phone']").setValue("+89991234567");
         form.$(".checkbox__box").click();
         //Selenide.sleep(10000);
         form.$(".button").click();
-        Selenide.sleep(1000);
+        //Selenide.sleep(1000);
         $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
 
     }
 
-       @Test
+    @Test
     void shouldGetWarningForIncorrectName() {
         //Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
         SelenideElement form = $("[enctype='application/x-www-form-urlencoded']");
         form.$("[name='name']").setValue("Иван11");
         form.$("[name='phone']").setValue("+89991234567");
@@ -47,7 +48,6 @@ public class CardOrderTest {
     @Test
     void shouldGetWarningForIncorrectPhone() {
         //Configuration.holdBrowserOpen = true;
-        open("http://localhost:9999");
         SelenideElement form = $("[enctype='application/x-www-form-urlencoded']");
         form.$("[name='name']").setValue("Иван Иванов");
         form.$("[name='phone']").setValue("08999123456");
@@ -56,4 +56,42 @@ public class CardOrderTest {
         form.$("[data-test-id=\"phone\"].input_invalid span span.input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
+
+    @Test
+    void shouldGetRedTextIfCheckboxIsntSelect() {
+        //Configuration.holdBrowserOpen = true;
+        SelenideElement form = $("[enctype='application/x-www-form-urlencoded']");
+        form.$("[name='name']").setValue("Анна-Мария Иванова-Редгрейв");
+        form.$("[name='phone']").setValue("+89991234567");
+        //form.$(".checkbox__box").click();
+        //Selenide.sleep(10000);
+        form.$(".button").click();
+        $(".checkbox.input_invalid").shouldBe(visible);
+
+    }
+
+    @Test
+    void shouldGetWarningIfNameIsntFilling() {
+        //Configuration.holdBrowserOpen = true;
+        SelenideElement form = $("[enctype='application/x-www-form-urlencoded']");
+        //form.$("[name='name']").setValue("Иван Иванов");
+        form.$("[name='phone']").setValue("+89991234567");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        form.$("[data-test-id=\"name\"].input_invalid span span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+
+    }
+
+    @Test
+    void shouldGetWarningIfPhoneIsntFilling() {
+        //Configuration.holdBrowserOpen = true;
+        SelenideElement form = $("[enctype='application/x-www-form-urlencoded']");
+        form.$("[name='name']").setValue("Иван Иванов");
+        //form.$("[name='phone']").setValue("08999123456");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        form.$("[data-test-id=\"phone\"].input_invalid span span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+
+    }
+
 }
